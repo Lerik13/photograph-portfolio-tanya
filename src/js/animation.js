cac2762aaea9a42b2ecccb36b271a5aa09d6ed09
Valeria <lerik13@gmail.com>
2023-08-23 00:06:52 -0600

    section 'Format photoshoot'

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
-		console.log(entry)
 		if (entry.isIntersecting) {
 			entry.target.classList.add('animate-show')
 		}/* else { // for multiple times animation
---
+++ src/js/tabs.js
@@ -0,0 +1,27 @@
+let tabs = document.querySelectorAll(".tab")
+let panels = document.querySelectorAll(".tab-panel")
+
+tabs.forEach(tab => {
+	tab.addEventListener("click", ()=>{
+		let tabTarget = tab.getAttribute("aria-controls")
+
+		tabs.forEach(t => {
+			t.classList.remove("border-brightRed");
+			t.classList.add("border-veryLightGray");
+			t.classList.remove("text-brightRed");
+		})
+		tab.classList.add("border-brightRed");
+		tab.classList.add("text-brightRed");
+
+		panels.forEach(panel =>{
+			let panelId = panel.getAttribute("id")
+			if (tabTarget === panelId){
+				panel.classList.remove("invisible", "opacity-0")
+				panel.classList.add("visible", "opacity-100")
+
+			} else {
+				panel.classList.add("invisible", "opacity-0")
+			}
+		})
+	})
+})
